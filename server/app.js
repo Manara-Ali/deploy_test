2842808ce1088381a03bc838bae9c26a6159e8a8
Manara-Ali <manaraali64@gmail.com>
2024-08-12 12:07:04 -0400

    added cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const productRouter = require("./routes/productRouter");
 
 const app = express();
+
+app.use(cors());
 
 app.use("/api/v1/products", productRouter)
 
